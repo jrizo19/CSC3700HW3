f68dfe1e720c37a4187b4d6ba4122a11143c1137
jrizo19 <89281518+jrizo19@users.noreply.github.com>
2023-10-31 00:35:46 -0500

    Setup Complete
    All necessary directories and files within directories were created, and just need to be written. The node app is set up and working, along with a generic Page Not Found screen.

--- HW3/hw3app.js
+++ HW3/hw3app.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const app = express();
-app.set( 'view engine', 'pug'); // set engine
-app.set( 'views', 'views'); // set views
+app.set('view engine', 'pug');
+app.set('views', 'views');
 const db = require("./util/database");
-db.execute( "select * from customer")
-    .then( result => {
-        console.log( "Results=");
-        console.log( result );
-    })
-    .catch( err => {
-        console.log( "DB ERR:"); console.log( err );
-    })
+// db.execute( "select * from customer") Test For Database connection Successful
+//     .then( result => {
+//         console.log( "Results=");
+//         console.log( result );
+//     })
+//     .catch( err => {
+//         console.log( "DB ERR:"); console.log( err );
+//     })
+const bodyParser = require("body-parser");
+const path = require("path");
+const http = require("http");
+
+app.use( bodyParser.urlencoded({extended: false})); // middleware for body
+app.use( express.static( path.join(__dirname, 'public')));
+
+app.get('*', function(req, res){ // page not found
+    res.render( 'pageNotFound', {
+        title:"Page Not Found!",
+        subtitle:"Surely this is your fault, not mine...",
+    });
+})
+
+let port = 1030;
+const server = http.createServer(app);
+server.listen( port );
+console.log( `Listening on http://localhost:${port}`);
